1b8eef2f09ed9abcba52057f12b5afe9e1fcd680
Sebastian Friedel <sfriedel@users.noreply.github.com>
2015-07-19 19:45:29 +0200

    improved tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,16 +2,25 @@ extern crate lexer;
 use lexer::{Item, StateFn, Lexer};
 
 
+const DATA: &'static str = "foo,bar,baz // some comment";
+
+
 #[derive(Debug, PartialEq)]
 pub enum ItemType {
     Comma,
     Text,
+    Comment,
     EOF
 }
 
 
 fn lex_text(l: &mut Lexer<ItemType>) -> Option<StateFn<ItemType>> {
     loop {
+        if l.remaining_input().starts_with("//") {
+            l.finish_started(ItemType::Text);
+            l.next();
+            return Some(StateFn(lex_comment));
+        }
         match l.peek() {
             None => break,
             Some(',') => {
@@ -29,16 +38,35 @@ fn lex_text(l: &mut Lexer<ItemType>) -> Option<StateFn<ItemType>> {
 }
 
 
+fn lex_comment(l: &mut Lexer<ItemType>) -> Option<StateFn<ItemType>> {
+    loop {
+        match l.peek() {
+            None => break,
+            Some('\n') => {
+                l.finish_started(ItemType::Comment);
+                l.next();
+                return Some(StateFn(lex_text));
+            },
+            Some(_) => {},
+        }
+        l.next();
+    }
+    l.finish_started(ItemType::Comment);
+    l.emit(ItemType::EOF);
+    None
+}
+
+
 #[test]
 fn test_lexer() {
-    let data = "foo,bar,baz";
-    let items = lexer::lex(&data, lex_text);
+    let items = lexer::lex(DATA, lex_text);
     let expected_items = vec!(
         Item{typ: ItemType::Text, val: "foo"},
         Item{typ: ItemType::Comma, val: ","},
         Item{typ: ItemType::Text, val: "bar"},
         Item{typ: ItemType::Comma, val: ","},
-        Item{typ: ItemType::Text, val: "baz"},
+        Item{typ: ItemType::Text, val: "baz "},
+        Item{typ: ItemType::Comment, val: "// some comment"},
         Item{typ: ItemType::EOF, val: ""}
     );
 
