1da422797bcdc7e4ed2e0e95f775f51214159b83
Sebastian Friedel <sfriedel@users.noreply.github.com>
2015-09-25 08:51:24 +0200

    removed usage of chars iterator and converted to next() instead of peek() based access

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-use std::str::Chars;
-use std::iter::Peekable;
-
-
 pub struct StateFn<T: PartialEq>(pub fn(&mut Lexer<T>) -> Option<StateFn<T>>);
 
 
@@ -14,9 +10,9 @@ pub struct Item<'a, T> {
 
 pub struct Lexer<'a, T: PartialEq> {
     input: &'a str,
-    chars_iter: Peekable<Chars<'a>>,
     start: usize,
     pos: usize,
+    width: usize,
     items: Vec<Item<'a, T>>
 }
 
@@ -25,9 +21,9 @@ impl<'a, T: PartialEq> Lexer<'a, T> {
     fn new(input: &'a str) -> Lexer<'a, T> {
         Lexer {
             input: input,
-            chars_iter: input.chars().peekable(),
             start: 0,
             pos: 0,
+            width: 0,
             items: Vec::new()
         }
     }
@@ -44,10 +40,16 @@ impl<'a, T: PartialEq> Lexer<'a, T> {
     }
 
     pub fn next(&mut self) -> Option<char> {
-        self.chars_iter.next().map(|c| {
-            self.pos += 1;
-            c
-        })
+        self.input[self.pos..].chars().next()
+            .map(|ch| {
+                self.width = ch.len_utf8();
+                self.pos += self.width;
+                ch
+            })
+            .or_else(|| {
+                self.width = 0;
+                None
+            })
     }
 
     pub fn ignore(&mut self) {
@@ -55,28 +57,44 @@ impl<'a, T: PartialEq> Lexer<'a, T> {
     }
 
     pub fn peek(&mut self) -> Option<char> {
-        self.chars_iter.peek().cloned()
+        let ch = self.next();
+        self.backup();
+        ch
+    }
+
+    pub fn backup(&mut self) {
+        self.pos -= self.width;
+        self.width = 0;
     }
 
     pub fn accept(&mut self, valid: &str) -> bool {
-        self.peek()
-            .map_or(false, |c| if valid.contains(c) { self.next(); true } else { false })
-    }
-
-    pub fn accept_run(&mut self, valid: &str) {
-        loop {
-            match self.peek() {
-                Some(c) => if valid.contains(c) { self.next(); } else { break; },
-                None => break,
+        match self.next() {
+            Some(ch) if valid.contains(ch) => true,
+            _ => {
+                self.backup();
+                false
             }
         }
     }
 
+    pub fn accept_run(&mut self, valid: &str) -> bool {
+        let mut found = false;
+        while let Some(ch) = self.next() {
+            if !valid.contains(ch) {
+                break;
+            };
+            found = true;
+        };
+        self.backup();
+        found
+    }
+
     pub fn emit(&mut self, typ: T) {
-        self.items.push(Item {
+        let item = Item {
             typ: typ,
             val: &self.input[self.start .. self.pos]
-        });
+        };
+        self.items.push(item);
         self.start = self.pos;
     }
 
@@ -84,6 +102,10 @@ impl<'a, T: PartialEq> Lexer<'a, T> {
         if self.pos > self.start {
             self.emit(typ);
         }
+    }
+
+    pub fn current(&self) -> &str {
+        &self.input[self.start .. self.pos]
     }
 
     pub fn remaining_input(&self) -> &str {
